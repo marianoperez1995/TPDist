e30f0269c17ec47dd1b943b21efc1d9cb10984db
Mariano <Mariano@10.100.78.244>
2017-05-30 21:32:07 -0300

    puti

--- tpo_repositorio/src/Exception/ClienteException.java
+++ tpo_repositorio/src/Exception/ClienteException.java
@@ -12,7 +12,7 @@ public class ClienteException extends Exception implements Serializable{
 	private static final long serialVersionUID = -1059463244209717071L;
 
 	public ClienteException(String mensaje){
-		super("hola");
+		super(mensaje);
 	}
 	
 
