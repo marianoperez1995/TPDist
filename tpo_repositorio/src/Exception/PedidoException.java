72ec79b391d179d689f2d00b40302d50f02977fd
plc <plc@192.168.1.4>
2017-07-01 22:27:04 -0300

    Exceptions vacias

--- tpo_repositorio/src/Exception/PedidoException.java
+++ tpo_repositorio/src/Exception/PedidoException.java
@@ -3,6 +3,11 @@ package Exception;
 import java.io.Serializable;
 
 public class PedidoException extends Exception implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7931764663581285130L;
+
 	public PedidoException(String mensaje){
 		super(mensaje);
 	}
